d6478bcf94975ec0de65d30fd1f4e616b8fdc544
unknown <haithem.turki@gmail.com>
2018-07-03 19:23:50 +0100

    page connection et register

--- register.php
+++ register.php
@@ -3,32 +3,29 @@
   <head>
 
     
-    <title>page d'inscription'</title>
+    <title>page d'inscription</title>
 
     <!-- Plugin CSS -->
     <!-- <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">-->
     <!-- Custom styles for this template -->
-    <link href="css/login.css" rel="stylesheet">
+    <link href="css/register.css" rel="stylesheet">
     <script src="js/Easeijs.js"></script>
 
   </head>
 
   <body>
   <canvas id="projector">Your browser does not support the Canvas element.</canvas>
- <div class="login-form">
+ <div class="register-form">
     <h1>Register Form</h1>
-    <form action="#">
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <span>
-            <input type="checkbox" name="checkbox">
-            <label for="checkbox">remember</label>
-        </span>
-        <input type="submit" value="Se connecter" id="login">
+    <form action="register-verif.php" method="post">
+        Pseudo : <input type="text" name="username">
+        Mot de passe :   <input type="password" name="password">
+        Email : <input type="text" name="email">
+        <input type="submit" value="S'enregistrer" name="register">
     </form>
 </div>
   </body>
     <!-- Custom scripts for this template -->
-    <script src="js/animation.js"></script>
-    <script src="js/login.js"></script>
+    <!--<script src="js/animation.js"></script>-->
+   <!-- <script src="js/login.js"></script>-->
     </html>
